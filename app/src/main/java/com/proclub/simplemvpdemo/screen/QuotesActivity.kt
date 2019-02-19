b9c0069f157dece0ae986f1aca190ad9c41a3006
Setyo Nugroho <setyo.nugroho1337@gmail.com>
2019-02-19 15:03:53 +0700

    fix layout

--- app/src/main/java/com/proclub/simplemvpdemo/screen/QuotesActivity.kt
+++ app/src/main/java/com/proclub/simplemvpdemo/screen/QuotesActivity.kt
@@ -24,6 +24,7 @@ class QuotesActivity: AppCompatActivity(), QuotesView {
     private fun initListener() {
         buttonAdd.setOnClickListener {
             presenter.addQuotes(textQuote.text.toString())
+            textQuote.text.clear()
         }
 
         buttonClear.setOnClickListener {
